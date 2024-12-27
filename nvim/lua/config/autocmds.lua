f8804395b1edd93ce65a46e3b3415aaaf1244bb5
Sam Saffron <sam.saffron@gmail.com>
2024-12-27 16:46:28 +1100

    attempt to get autospec going

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,31 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function notify_file_change()
+  local notify = vim.fn.getcwd() .. "/bin/notify_file_change"
+
+  if vim.fn.executable(notify) == 0 then
+    -- Assuming you're using a Rails plugin that provides similar functionality
+    -- You might need to adjust this part based on your actual Rails setup
+    local root = vim.fn.exists("*rails#app") == 1 and vim.fn.rails.app().path() or ""
+    notify = root .. "/bin/notify_file_change"
+  end
+
+  if vim.fn.executable(notify) == 0 then
+    notify = vim.fn.getcwd() .. "../../bin/notify_file_change"
+  end
+
+  if vim.fn.executable(notify) == 1 then
+    if vim.fn.executable("socat") == 1 then
+      local cmd = notify .. " " .. vim.fn.expand("%:p") .. " " .. vim.fn.line(".")
+      vim.fn.system(cmd)
+    end
+  end
+end
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*",
+  callback = function()
+    notify_file_change()
+  end,
+})
