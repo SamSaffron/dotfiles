b3be1c9e07e4c5e9bc4070ad877d7a63657568a2
Sam Saffron <sam.saffron@gmail.com>
2025-01-28 10:41:41 +1100

    add gist, open output panel for testing on the right

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -515,6 +515,9 @@ return {
 						rspec_cmd = "/tmp/d-rspec",
 					}),
 				},
+				output_panel = {
+					open = "botright vsplit | vertical resize 80",
+				},
 			})
 		end,
 	},
@@ -611,5 +614,20 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 		opts = {},
+	},
+	{
+		"Rawnly/gist.nvim",
+		cmd = { "GistCreate", "GistCreateFromFile", "GistsList" },
+		config = true,
+	},
+	-- `GistsList` opens the selected gif in a terminal buffer,
+	-- nvim-unception uses neovim remote rpc functionality to open the gist in an actual buffer
+	-- and prevents neovim buffer inception
+	{
+		"samjwill/nvim-unception",
+		lazy = false,
+		init = function()
+			vim.g.unception_block_while_host_edits = true
+		end,
 	},
 }
