a11ac3b01f563c4d8f2d9ce5297b3ca0d1a9014e
Sam Saffron <sam.saffron@gmail.com>
2025-11-12 12:03:18 +1100

    unlock package

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -462,7 +462,7 @@ return {
   },
   {
     "nvim-neotest/neotest",
-    commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
+    --- commit = "52fca6717ef972113ddd6ca223e30ad0abb2800c",
     lazy = true,
     dependencies = {
       "olimorris/neotest-rspec",
