d4d72ac2807f0f587b9fa8e65cc5d9b105ca5df4
Sam Saffron <sam.saffron@gmail.com>
2025-11-12 12:29:52 +1100

    change should apply per workspace not globally

--- hypr/toggle-master-layout.sh
+++ hypr/toggle-master-layout.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
-# Get current layout
-current_layout=$(hyprctl getoption general:layout | grep -oP 'str: \K\w+')
+# Get current workspace
+workspace=$(hyprctl activeworkspace -j | jq -r '.id')
 
-# If we're in dwindle, switch to master left
-if [ "$current_layout" = "dwindle" ]; then
-  hyprctl keyword general:layout master
-  hyprctl keyword master:mfact 0.6
-  hyprctl keyword master:slave_count_for_center_master -1
-  hyprctl keyword master:orientation left
+# Get current workspace layout
+workspace_layout=$(hyprctl activeworkspace -j | jq -r '.lastwindow' | xargs -I {} hyprctl clients -j | jq -r ".[] | select(.address == \"{}\") | .workspace.name")
+current_layout=$(hyprctl activeworkspace -j | jq -r '.name')
+
+# Check if workspace has custom layout by trying to get layout messages
+# We'll track state in a temporary file per workspace
+state_file="/tmp/hypr-layout-state-ws${workspace}"
+
+if [ ! -f "$state_file" ]; then
+  echo "master-center" > "$state_file"
+fi
+
+current_state=$(cat "$state_file")
+
+if [ "$current_state" = "dwindle" ]; then
+  # Switch to master left
   hyprctl dispatch layoutmsg orientationleft
   hyprctl dispatch layoutmsg mfact exact 0.6
+  echo "master-left" > "$state_file"
   echo "Switched to 2-window layout (left orientation)"
   notify-send -t 1000 "Layout: Master Left" "2-window layout"
+elif [ "$current_state" = "master-left" ]; then
+  # Switch to master center
+  hyprctl dispatch layoutmsg orientationcenter
+  hyprctl dispatch layoutmsg mfact exact 0.4
+  echo "master-center" > "$state_file"
+  echo "Switched to 3-window layout (center orientation)"
+  notify-send -t 1000 "Layout: Master Center" "3-window layout"
 else
-  # We're in master layout, check orientation
-  current_orientation=$(hyprctl getoption master:orientation | grep -oP 'str: \K\w+')
-  echo "Current orientation: $current_orientation"
-
-  if [ "$current_orientation" = "center" ]; then
-    # Switch from center to dwindle
-    hyprctl keyword general:layout dwindle
-    echo "Switched to dwindle layout"
-    notify-send -t 1000 "Layout: Dwindle" "Tiling layout"
-  else
-    # Switch from left to center
-    hyprctl keyword master:mfact 0.4
-    hyprctl keyword master:slave_count_for_center_master 0
-    hyprctl keyword master:orientation center
-    hyprctl dispatch layoutmsg orientationcenter
-    hyprctl dispatch layoutmsg mfact exact 0.4
-    echo "Switched to 3-window layout (center orientation)"
-    notify-send -t 1000 "Layout: Master Center" "3-window layout"
-  fi
+  # Switch back to dwindle (reset to default)
+  hyprctl dispatch layoutmsg orientationleft
+  hyprctl dispatch layoutmsg mfact exact 0.5
+  echo "dwindle" > "$state_file"
+  echo "Switched to dwindle layout"
+  notify-send -t 1000 "Layout: Dwindle" "Tiling layout"
 fi
