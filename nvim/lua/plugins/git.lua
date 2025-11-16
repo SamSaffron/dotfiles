331b23adaf8632dfc00d265bc4266bf8291c1c10
Sam Saffron <sam.saffron@gmail.com>
2025-11-17 09:51:51 +1100

    add another shortcut

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -34,10 +34,9 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      { "<leader>lg", "<cmd>LazyGit<cr>",                  desc = "LazyGit" },
+      { "<leader>lf", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit" }
     }
   }
 }
