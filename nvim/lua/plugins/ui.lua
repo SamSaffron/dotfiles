1976b098b95d3d14264f2981dd405492c000e69d
Sam Saffron <sam.saffron@gmail.com>
2025-11-19 11:46:27 +1100

    lets show plugins for now

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -144,6 +144,16 @@ return {
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_gitignored = true,
+          always_show_by_pattern = {
+            "*plugins*",
+          },
+        },
+      },
+    },
     keys = {
       {
         "<leader>f",
