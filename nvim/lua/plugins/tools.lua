fe7f2530e4bf4bb9993a294a43ae16855f09555b
Sam Saffron <sam.saffron@gmail.com>
2025-11-20 11:33:42 +1100

    fm to show all the files that changed since main with diff to main

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -13,6 +13,41 @@ return {
       { "<leader>fb", "<cmd>Telescope buffers<CR>",                                    desc = "Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<CR>",                                  desc = "Help tags" },
       { "<leader>fo", "<cmd>Telescope oldfiles<CR>",                                   desc = "Recent files" },
+      {
+        "<leader>fm",
+        function()
+          local previewers = require("telescope.previewers")
+          local pickers = require("telescope.pickers")
+          local sorters = require("telescope.sorters")
+          local finders = require("telescope.finders")
+
+          local branch = "main"
+          if vim.fn.system("git rev-parse --verify main 2>/dev/null") == "" then
+            branch = "master"
+          end
+
+          local merge_base = vim.fn.system("git merge-base " .. branch .. " HEAD"):gsub("\n", "")
+          if merge_base == "" then
+            merge_base = branch
+          end
+
+          pickers.new({}, {
+            prompt_title = "Modified (vs " .. branch .. " base)",
+            finder = finders.new_oneshot_job({ "sh", "-c", "git diff --name-only --relative " .. merge_base .. " && git ls-files --others --exclude-standard" }),
+            sorter = sorters.get_fuzzy_file(),
+            previewer = previewers.new_termopen_previewer({
+              get_command = function(entry)
+                if vim.fn.system("git ls-files --error-unmatch " .. vim.fn.shellescape(entry.value) .. " 2>/dev/null") ~= "" then
+                  return { "git", "diff", merge_base, "--", entry.value }
+                else
+                  return { "git", "diff", "--no-index", "/dev/null", entry.value }
+                end
+              end
+            })
+          }):find()
+        end,
+        desc = "Changed files (main)"
+      },
       { "<leader>fw", "<cmd>Telescope git_status<CR>",                                 desc = "Git changed files" },
     },
     config = function()
