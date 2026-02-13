1f7e6d67b1ec9f04c7f3032b814578d2a28432a2
Sam Saffron <sam.saffron@gmail.com>
2026-02-13 17:17:43 +1100

    various updates

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -426,6 +426,7 @@ return {
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
+    enabled = false,
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
