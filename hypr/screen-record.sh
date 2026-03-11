7addfaa4cda90015d7b99effe28ee97df2477f7c
Sam Saffron <sam.saffron@gmail.com>
2026-03-12 10:25:04 +1100

    swap to gpu screen recorder

--- hypr/screen-record.sh
+++ hypr/screen-record.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-PIDFILE="/tmp/wf-recorder.pid"
-RECORDING_FILE_PATH="/tmp/wf-recorder-current-file"
+PIDFILE="/tmp/gpu-screen-recorder.pid"
+RECORDING_FILE_PATH="/tmp/gpu-screen-recorder-current-file"
 SCREENCAST_DIR="$HOME/Videos/Screencasts"
+LOGFILE="/tmp/gpu-screen-recorder.log"
 
 mkdir -p "$SCREENCAST_DIR"
 
@@ -32,11 +33,21 @@ else
   GEOMETRY=$(slurp 2>/dev/null)
 
   if [ -z "$GEOMETRY" ] || [[ "$GEOMETRY" =~ ^0,0\ 0x0$ ]]; then
-    GEOMETRY=$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+    GEOMETRY=$(hyprctl activewindow -j | jq -r '"\(.size[0])x\(.size[1])+\(.at[0])+\(.at[1])"')
+  else
+    # Convert slurp format "X,Y WxH" to gpu-screen-recorder format "WxH+X+Y"
+    GEOMETRY=$(echo "$GEOMETRY" | sed 's/\([0-9]*\),\([0-9]*\) \([0-9]*\)x\([0-9]*\)/\3x\4+\1+\2/')
   fi
 
-  echo GEOMETRY: "$GEOMETRY"
+  gpu-screen-recorder -w region -region "$GEOMETRY" -f 30 -k h264 -q medium -o "$RECORDING_FILE" >"$LOGFILE" 2>&1 &
+  GSR_PID=$!
+  sleep 1
 
-  wf-recorder -g "$GEOMETRY" -c h264_nvenc -p preset=fast -p rc=vbr -p cq=35 -p b:v=1M -r 30 -f "$RECORDING_FILE" &
-  echo $! >"$PIDFILE"
+  if kill -0 "$GSR_PID" 2>/dev/null; then
+    echo "$GSR_PID" >"$PIDFILE"
+    notify-send "Screen Recording" "Recording started" -i media-record
+  else
+    rm -f "$RECORDING_FILE_PATH"
+    notify-send "Screen Recording" "gpu-screen-recorder failed to start. Check $LOGFILE" -i dialog-error
+  fi
 fi
