ad04cfbd69689ceec0894696d1fbbb7ebd10b83c
Sam Saffron <sam.saffron@gmail.com>
2026-08-13 14:15:07 +1000

    update to lua config

--- hypr/help.sh
+++ hypr/help.sh
@@ -7,7 +7,7 @@ require "time"
 TITLE = "Hyprland Shortcuts"
 APP_ID = ENV.fetch("HYPR_HELP_APP_ID", "io.hypr.help")
 
-DEFAULT_CFG = File.join(ENV.fetch("XDG_CONFIG_HOME", File.join(Dir.home, ".config")), "hypr", "hyprland.conf")
+DEFAULT_CFG = File.join(ENV.fetch("XDG_CONFIG_HOME", File.join(Dir.home, ".config")), "hypr", "hyprland.lua")
 cfg_env = ENV["HYPR_HELP_CONFIG"].to_s
 CFG = cfg_env.empty? ? DEFAULT_CFG : cfg_env
 
@@ -86,61 +86,155 @@ def pretty_mod(mod)
   mod_map.fetch(mod, mod)
 end
 
-def parse_binds(cfg_path)
-  var_re = /^\s*(\$\w+)\s*=\s*(.*?)\s*$/
-  bind_re = /^\s*(bindm|bindl|bindel|bind)\s*=\s*(.*?)\s*$/
+def split_lua(source, separator)
+  parts = []
+  start = 0
+  depth = 0
+  quote = nil
+  escaped = false
+  i = 0
 
+  while i < source.length
+    char = source[i]
+    if quote
+      if escaped
+        escaped = false
+      elsif char == "\\"
+        escaped = true
+      elsif char == quote
+        quote = nil
+      end
+    elsif char == '"' || char == "'"
+      quote = char
+    elsif "({[".include?(char)
+      depth += 1
+    elsif ")}]}".include?(char)
+      depth -= 1
+    elsif depth.zero? && source[i, separator.length] == separator
+      parts << source[start...i].strip
+      i += separator.length - 1
+      start = i + 1
+    end
+    i += 1
+  end
+
+  parts << source[start..].to_s.strip
+end
+
+def split_lua_comment(line)
+  code, comment = split_lua(line, "--")
+  [code, comment.to_s.strip]
+end
+
+def lua_value(expression, vars)
+  expression = expression.strip
+  concat = split_lua(expression, "..")
+  return concat.map { |part| lua_value(part, vars) }.join if concat.length > 1
+
+  if expression.match?(/\A["']/) && expression[-1] == expression[0]
+    expression[1...-1].gsub(/\\([\\"'])/, '\\1')
+  elsif (match = expression.match(/\A(\w+)\s*%\s*(\d+)\z/))
+    vars.fetch(match[1], match[1]).to_i % match[2].to_i
+  elsif expression.match?(/\A-?\d+\z/)
+    expression.to_i
+  else
+    vars.fetch(expression, expression)
+  end
+end
+
+def format_lua_arg(expression, vars)
+  expression = expression.to_s.strip
+  return "" if expression.empty?
+
+  if expression.start_with?("{") && expression.end_with?("}")
+    fields = split_lua(expression[1...-1], ",").filter_map do |field|
+      name, value = field.split("=", 2).map(&:strip)
+      next if value.nil?
+      "#{name}=#{lua_value(value, vars)}"
+    end
+    return fields.join(", ")
+  end
+
+  lua_value(expression, vars).to_s
+end
+
+def parse_lua_bind(line, vars)
+  code, desc = split_lua_comment(line)
+  match = code.strip.match(/\Ahl\.bind\((.*)\)\s*\z/)
+  return unless match
+
+  parts = split_lua(match[1], ",")
+  return if parts.length < 2
+
+  combo_parts = lua_value(parts[0], vars).to_s.split("+").map(&:strip)
+  key = pretty_key(combo_parts.pop.to_s)
+  mods = combo_parts.map { |mod| pretty_mod(mod) }
+
+  action_match = parts[1].match(/\Ahl\.dsp\.([\w.]+)\((.*)\)\z/)
+  action = action_match ? action_match[1] : parts[1]
+  arg = action_match ? format_lua_arg(action_match[2], vars) : ""
+
+  options = parts[2] || ""
+  bind_type = if options.match?(/\bmouse\s*=\s*true\b/)
+    "bindm"
+  elsif options.match?(/\brepeating\s*=\s*true\b/)
+    "bindel"
+  elsif options.match?(/\blocked\s*=\s*true\b/)
+    "bindl"
+  else
+    "bind"
+  end
+
+  {
+    type: bind_type,
+    key: (mods + [key]).join("+"),
+    action: action,
+    arg: arg,
+    desc: desc,
+  }
+end
+
+def parse_binds(cfg_path)
+  lines = File.readlines(cfg_path, mode: "r:UTF-8", invalid: :replace, undef: :replace)
   vars = {}
   binds = []
+  index = 0
 
-  File.foreach(cfg_path, mode: "r:UTF-8", invalid: :replace, undef: :replace) do |raw|
-    line = raw.chomp
-    stripped = line.strip
-    next if stripped.empty? || stripped.start_with?("#")
+  while index < lines.length
+    code, = split_lua_comment(lines[index])
 
-    code, comment = line.split("#", 2)
-    code = code.rstrip
-    desc = comment ? comment.strip : ""
+    if (loop_match = code.match(/^\s*for\s+(\w+)\s*=\s*(-?\d+)\s*,\s*(-?\d+)\s+do\s*$/))
+      loop_end = index + 1
+      loop_end += 1 until loop_end >= lines.length || lines[loop_end].match?(/^\s*end\s*(?:--.*)?$/)
+      range = loop_match[2].to_i..loop_match[3].to_i
 
-    if (m = code.match(var_re))
-      vars[m[1]] = m[2].strip
+      range.each do |value|
+        loop_vars = vars.merge(loop_match[1] => value)
+        lines[(index + 1)...loop_end].each do |body_line|
+          body_code, = split_lua_comment(body_line)
+          if (local_match = body_code.match(/^\s*local\s+(\w+)\s*=\s*(.*?)\s*$/))
+            loop_vars[local_match[1]] = lua_value(local_match[2], loop_vars)
+          elsif (bind = parse_lua_bind(body_line, loop_vars))
+            binds << bind
+          end
+        end
+      end
+
+      index = loop_end + 1
       next
     end
 
-    m = code.match(bind_re)
-    next unless m
-
-    bind_type = m[1]
-    rhs = m[2]
-    rhs = rhs.gsub(/\$\w+/) { |name| vars.fetch(name, name) }
-    parts = rhs.split(",").map(&:strip)
-    next if parts.length < 3
-
-    mods = parts[0]
-    key = parts[1]
-    action = parts[2]
-    arg = parts[3..].to_a.join(",").strip
-
-    mods_pretty = if mods.to_s.strip.empty?
-      ""
-    else
-      mods.split.map { |mod| pretty_mod(mod) }.join("+")
+    if (local_match = code.match(/^\s*local\s+(\w+)\s*=\s*(.*?)\s*$/))
+      vars[local_match[1]] = lua_value(local_match[2], vars)
+    elsif (bind = parse_lua_bind(lines[index], vars))
+      binds << bind
     end
 
-    key_pretty = pretty_key(key)
-    combo = mods_pretty.empty? ? key_pretty : "#{mods_pretty}+#{key_pretty}"
-
-    binds << {
-      type: bind_type,
-      key: combo,
-      action: action,
-      arg: arg,
-      desc: desc,
-    }
+    index += 1
   end
 
   type_order = { "bind" => 0, "bindm" => 1, "bindel" => 2, "bindl" => 3 }
-  binds.sort_by { |b| [type_order.fetch(b[:type], 99), b[:key].downcase, b[:action], b[:arg]] }
+  binds.sort_by { |bind| [type_order.fetch(bind[:type], 99), bind[:key].downcase, bind[:action], bind[:arg]] }
 end
 
 def clear_list(list)
